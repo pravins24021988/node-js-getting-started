f77412ab8d432de9205884bc5114d72d0533c60e
pravin <pravin.shendge@ericsson.com>
2021-12-24 16:08:59 +0530

    Add cool face API

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-const { Client } = require('pg');
-const cool = require('cool-ascii-faces');
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
-
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  });
-
-const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -35,7 +16,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
@@ -48,14 +28,5 @@ express()
       res.send("Error " + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
-showTimes = () => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-}
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
